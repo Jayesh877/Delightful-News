18706104040773cd6aea43bd96d5a0351dde5fae
Divya Chhabra <J1202chhabra@gamil.com>
2022-10-08 00:13:10 +0530

    updated code

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+  // constructor()
+  // {
+  //   super();
+  //   console.log("My cunstructor");
+  // }
   render() {
+    let {title,description,myurl,url}= this.props;
     return (
-      <div>This is a NewsItem</div>
+        <div >
+        <div className="card" style={{width: "18rem"}}>
+  <img src={myurl} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{title}...</h5>
+    <p className="card-text">{description}...</p>
+    <a href={url} target="_blank" rel="noreferrer" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+      
     )
   }
 }
