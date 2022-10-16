cdc7fb268a07e0ff5546e53eebd43134cb79e56c
Divya Chhabra <J1202chhabra@gamil.com>
2022-10-16 19:45:16 +0530

    updated code

--- src/component/News.js
+++ src/component/News.js
@@ -3,66 +3,87 @@ import NewsItem from './NewsItem'
 
 export class News extends Component {
   articles=[
-    {
-        "source": {
-            "id": "espn-cric-info",
-            "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-        "publishedAt": "2020-04-27T11:41:47Z",
-        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-        "source": {
-            "id": "espn-cric-info",
-            "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-        "publishedAt": "2020-03-30T15:26:05Z",
-        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
 ];
   constructor()
   {
     super();
     this.state=
   {    
-    articles:this.articles
+    articles:this.articles,
+    page:1,
+    totalResult:0
   }
   };
   async componentDidMount()
   {
-      let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e2483041be8d415bab21708537c62960";
+      let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e2483041be8d415bab21708537c62960&pageSize=21";
       let data= await fetch(url);
       
       let parsedData=  await data.json()
-      console.log(parsedData);
+      console.log(this.state.page);
       this.setState({
-        articles:parsedData.articles
+        articles:parsedData.articles,
+        page:1,
+        totalResult:parsedData.totalResult
       })
+  }
+  next =async()=>
+  {
+    if(this.state.page+1>Math.ceil(this.state.totalResult/21))
+    {
+
+    }
+    else
+    {
+      let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e2483041be8d415bab21708537c62960&pageSize=21&page=${this.state.page+1}`;
+      let data= await fetch(url);
+      
+      let parsedData=  await data.json()
+      // console.log(parsedData);
+    this.setState(
+      {
+        articles:parsedData.articles,
+        page:this.state.page +1
+      }
+    )
+    }
+      
+    console.log("next"+this.totalResult)
+
+  }
+  prev =async()=>
+  {
+    let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e2483041be8d415bab21708537c62960&pageSize=21&page=${this.state.page-1}`;
+      let data= await fetch(url);
+      
+      let parsedData=  await data.json()
+      // console.log(parsedData);
+    this.setState(
+      {
+        articles:parsedData.articles,
+        page:this.state.page - 1
+      }
+    )
+    console.log("prev")
   }
   
   render() {
+  
     return (
       <>
       <div className="container my-4" >
         <h2>This a news app</h2>
-      <div className="row my-3">
+      <div className="row my-3 mx-2">
       { this.state.articles.map((element)=>{
-        return <div className="col-md-3" key={element.url}>
+        return <div className="col-md-3 mx-2 my-2" key={element.url}>
             <NewsItem title={element.title} description={element.description} myurl={element.urlToImage} url={element.url}/>
         </div>}
         ) 
-      }
-       
+      }    
+      <div className="container d-flex justify-content-between">
+      <button onClick={this.prev} disabled={this.state.page<=1} type="button" className="btn btn-warning">&#8592; Previous</button>
+      <button onClick={this.next} disabled={this.state.page>=4}type="button" className="btn btn-info">Next &#8594;	</button>
+      </div>
       </div>
       </div>
       </>
--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -6,12 +6,13 @@ export class NewsItem extends Component {
   //   super();
   //   console.log("My cunstructor");
   // }
+  
   render() {
     let {title,description,myurl,url}= this.props;
     return (
         <div >
         <div className="card" style={{width: "18rem"}}>
-  <img src={myurl} className="card-img-top" alt="..."/>
+  <img src={!myurl?"https://image.cnbcfm.com/api/v1/image/107132885-1665523655070-gettyimages-1243437689-ad_r1_07141.jpeg?v=1665572401&w=1920&h=1080":myurl} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}...</h5>
     <p className="card-text">{description}...</p>
